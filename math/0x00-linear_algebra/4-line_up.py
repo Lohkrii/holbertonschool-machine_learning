48ef8744f7481af150a68bf216f850d9623a5eaa
Joshua Lopez <joshualopez0429@gmail.com>
2021-11-29 21:28:59 -0400

    Add 2 matrices if they are the same shape.

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import numpy as np
+def add_arrays(arr1, arr2):
+    """
+    Function that adds two arrays
+    """
+    if np.array(arr1).shape == np.array(arr2).shape:
+        return np.add(arr1, arr2)
+    else:
+        return None
