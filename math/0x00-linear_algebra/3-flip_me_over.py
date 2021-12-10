7f5056ac458c4f2234cb2971f20ab5337ab2e778
Joshua E. López Carrears <joshua@consiliencecap.com>
2021-12-10 12:49:06 -0400

    Fixed code and pycodestyle

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-import numpy as np
-
+""" Flip me over """
 
 def matrix_transpose(matrix):
     """
     Function that returns the transpose of a matrix:
     """
-    return np.transpose(matrix)
+    matrix_result = [[0 for i in matrix] for j in matrix[0]]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            matrix_result[j][i] = matrix[i][j]
+    return matrix_result
