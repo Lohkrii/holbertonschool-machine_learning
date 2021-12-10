f5965152defcf026f1eaa0bd9db92e6b6a142ebc
Joshua E. López Carrears <joshua@consiliencecap.com>
2021-12-10 13:48:09 -0400

    Pycode style fix

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,7 +6,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Function that concatenates two matrices along a specific axis.
     """
-    if len(mat1[0]) == len(mat2[0]) and axis == 0 :
+    if len(mat1[0]) == len(mat2[0]) and axis == 0:
         return mat1 + mat2
     elif axis == 1 and len(mat1) == len(mat2):
         matrix = []
